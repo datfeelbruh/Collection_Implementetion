86c34afc12a05471c92f4c90be1488369d2bade7
khoroshko.d <datfeelman@gmail.com>
2022-07-21 15:01:26 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
