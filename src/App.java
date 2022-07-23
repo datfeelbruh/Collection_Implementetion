c5c8c9b07369b6bd7ea14fe04a033b4e525ae46c
khoroshko.d <datfeelman@gmail.com>
2022-07-23 17:08:17 +0300

    init

--- src/App.java
+++ src/App.java
@@ -1,11 +1,26 @@
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import static org.junit.Assert.assertEquals;
 
 public class App {
     public static void main(String[] args) {
-
+        final LinkedList<String> testInstance = new LinkedList<>() {
+            {
+                add("a");
+                add("b");
+                add("c");
+                add("d");
+                add("e");
+            }
+        };
+        final ListIterator<String> listIterator = testInstance.listIterator(5);
+        assertEquals(5, listIterator.nextIndex());
+        assertEquals("e", listIterator.previous());
+        assertEquals(4, listIterator.nextIndex());
+        assertEquals("e", listIterator.next());
+        assertEquals(5, listIterator.nextIndex());
     }
 }
